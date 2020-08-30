3552d4b89422037a8a92df450e156ab23272389e
Xchel Sanchez <xchelsvz@gmail.com>
2020-08-29 21:07:02 -0500

    feat: get products from cambio fisico

--- functions/product_incidences_functions.js
+++ functions/product_incidences_functions.js
@@ -8,13 +8,15 @@ exports.get_product_from_ticket_venta = (json_file) => {
             if (transaction.encabezado[0].ENCDescripcion[0] === 'Ticket Venta'){
                 const current_store = transaction.encabezado[0].ALMCodigo[0];
                 const transaction_hour = transaction.encabezado[0].ENCHoraTrx[0];
+                const transaction_date = transaction.encabezado[0].ENCFechaTrx[0];
+                const date_splitted = transaction_date.split('/');
                 for (let i = 0; i < transaction.detalle[0].items[0].item.length; i++) {
                     let product = { id: '', reference:'', store: current_store, quantity: 0 };
                     if (transaction.detalle[0].items[0].item[i]['$'].Tipo === 'Referencia'){
                         const reference_consec = transaction.detalle[0].items[0].item[i]['$'].nitem;
                         product.reference = transaction.detalle[0].items[0].item[i].REFCodigo1[0];
                         product.quantity = parseInt(transaction.detalle[0].items[0].item[i].IRFCantidad);
-                        product.id = `${current_store}-${reference_consec}-${transaction_hour}-${transaction.detalle[0].items[0].item[i].REFCodigo1[0]}`;
+                        product.id = `Ticket Venta-${current_store}-${reference_consec}-${date_splitted[0]}-${date_splitted[1]}-${date_splitted[2]}-${transaction_hour}-${transaction.detalle[0].items[0].item[i].REFCodigo1[0]}`;
                         products.push(product);
                     }
                 }
@@ -25,6 +27,36 @@ exports.get_product_from_ticket_venta = (json_file) => {
     catch(error){
         console.log(error)
         logger.error(`get_product_from_ticket_venta function, error: ${JSON.stringify(error)}`);
+        return [];
+    }
+}
+
+exports.get_product_from_cambio_fisico= (json_file) => {
+    try{
+        let products = [];
+        json_file.transacciones.transaccion.forEach(transaction => {
+            if (transaction.encabezado[0].ENCDescripcion[0] === 'Cambio Fisico por talla'){
+                const current_store = transaction.encabezado[0].ALMCodigo[0];
+                const transaction_hour = transaction.encabezado[0].ENCHoraTrx[0];
+                const transaction_date = transaction.encabezado[0].ENCFechaTrx[0];
+                const date_splitted = transaction_date.split('/');
+                for (let i = 0; i < transaction.detalle[0].items[0].item.length; i++) {
+                    let product = { id: '', reference:'', store: current_store, quantity: 0 };
+                    if (transaction.detalle[0].items[0].item[i]['$'].Tipo === 'Referencia'){
+                        const reference_consec = transaction.detalle[0].items[0].item[i]['$'].nitem;
+                        product.reference = transaction.detalle[0].items[0].item[i].REFCodigo1[0];
+                        product.quantity = parseInt(transaction.detalle[0].items[0].item[i].IRFCantidad);
+                        product.id = `Cambio Fisico por talla-${current_store}-${reference_consec}-${date_splitted[0]}-${date_splitted[1]}-${date_splitted[2]}-${transaction_hour}-${transaction.detalle[0].items[0].item[i].REFCodigo1[0]}`;
+                        products.push(product);
+                    }
+                }
+            }
+        });
+        return products;
+    }
+    catch(error){
+        console.log(error)
+        logger.error(`get_product_from_cambio_fisico function, error: ${JSON.stringify(error)}`);
         return [];
     }
 }
